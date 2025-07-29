688504d1ca23c07c6832857523891e27229a0c25
ilafper <ivanaalfaya@gmail.com>
2025-07-29 13:16:21 +0200

    minecraft tfg

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    const cartasInfo = [
+  {
+    nombre: "Dragón de Fuego",
+    info: "Un poderoso dragón que escupe fuego y arrasa todo a su paso.",
+    rareza: "Legendaria"
+  },
+  {
+    nombre: "Guerrero Elfo",
+    info: "Ágil y certero, experto en el combate a distancia con su arco.",
+    rareza: "Rara"
+  },
+  {
+    nombre: "Mago Oscuro",
+    info: "Dominador de las artes oscuras, capaz de lanzar hechizos devastadores.",
+    rareza: "Épica"
+  },
+  {
+    nombre: "Lobo Fantasma",
+    info: "Criatura etérea que ataca desde las sombras.",
+    rareza: "Infrecuente"
+  },
+  {
+    nombre: "Soldado Raso",
+    info: "Combatiente común, útil en grandes números.",
+    rareza: "Común"
+  }
+];
+
+
+
+});
+
+
